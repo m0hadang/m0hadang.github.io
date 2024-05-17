f38fc832f5bdd554497a87d5a8af8092bbb98108
mohadang <ehdlaos@gmail.com>
2024-05-18 02:15:22 +0900

    [mod] refactoring(String -> format!)

--- md-to-html/src/main.rs
+++ md-to-html/src/main.rs
@@ -1,28 +1,28 @@
-use markdown::ParseOptions;
-
 fn create_html_file(markdown_file: &str, html_file: &str) -> Result<(), markdown::message::Message> {
     let markdown = std::fs::read_to_string(markdown_file).unwrap();
-    let html = markdown::to_html_with_options(
+    let body = markdown::to_html_with_options(
         &markdown,
         &markdown::Options::gfm()
     )?;
 
-    let mut html_data = String::new();
-    html_data.push_str("<HTML>");
-    html_data.push_str( r#"<HEAD>
+    let html = format!(
+r#"<!DOCTYPE html>
+<HTML>
+<HEAD>
 <meta charset="UTF-8">
-    <link rel="stylesheet" href="css\style.css">
-    <title>Document</title>
-</HEAD>"#);
+  <link rel="stylesheet" href="css\style.css">
+  <title>Document</title>
+</HEAD>
 
-    html_data.push_str("\n");
-    html_data.push_str("<BODY>\n");
-    html_data.push_str(&html);
-    html_data.push_str("\n");
-    html_data.push_str("</BODY>\n");
+<BODY>
 
-    html_data.push_str("</HTML>\n");
-    std::fs::write(html_file, html_data).unwrap();
+{body}
+
+</BODY>
+
+</HTML>"#);
+
+    std::fs::write(html_file, html).unwrap();
     Ok(())
 }
 
